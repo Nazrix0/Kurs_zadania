68aea8eb867f72cb74fe435b7ae25ca353f55b18
tom <dawid.las123@gmail.com>
2022-12-13 14:03:24 +0000

    wa

--- ex2/top_df_local.sh
+++ ex2/top_df_local.sh
@@ -8,6 +8,7 @@ mkdir "$folder_name"
 
 "$command_1" >> "./$folder_name/$file_name" 2>&1
 "$command_2" >> "./$folder_name/$file_name" 2>&1
+
 export folder_name
 export file_name
 
--- ex2/top_df_remote.sh
+++ ex2/top_df_remote.sh
@@ -1,18 +1,15 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 TARGET=192.168.56.123
 
-file_torun_remote=top_df_local.sh
+file_torun_remote="top_df_local.sh"
 
 ping $TARGET -c1 -t1 > /dev/null 2>&1
 res=$?
 if [ "$res" == 1 ];then
     echo "Target $TARGET is not reachable"
-    exit -1
+    exit 1
 else
     scp  "$file_torun_remote" $TARGET:
     ssh $TARGET './$file_torun_remote; source top_df_local.sh; scp "./$folder_name/$file_name" 192.168.56.102:./code/ex2/'
 fi
-
-
-
